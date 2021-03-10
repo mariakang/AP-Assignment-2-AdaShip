255da3be582171ee65ea399c4dee2d8cfc77dde9
Maria Kang <maria.kang@ada.ac.uk>
2021-03-10 12:29:31 +0000

    Tidy up Fleet.

--- Fleet.cpp
+++ Fleet.cpp
@@ -1,18 +1,25 @@
 #include "Boat.h"
 #include "Fleet.h"
 
+/** Returns a copy of the fleet. */
 Fleet Fleet::copy() {
+  // instantiate a new Fleet object
   Fleet copy;
+  // copy over the attributes
   copy.size_ = size_;
   for (int i = 0; i < size_; i++) {
     copy.boats_[i] = boats_[i].copy();
   }
+  // return the new object
   return copy;
 }
 
+/** Adds a Boat object to the fleet. */
 void Fleet::add(Boat& boat) {
+  // check that we have capacity to add another boat
   if (size_ < MAX_BOATS) {
+    // add the new boat to the end, and increment the size
+    boats_[size_] = boat;
     size_++;
-    boats_[size_ - 1] = boat;
   }
 }
--- Fleet.h
+++ Fleet.h
@@ -1,44 +1,50 @@
 #pragma once
 
-using namespace std;
-
 #include "Boat.h"
 
 #define MAX_BOATS 80
 
+/** A collection of Boat objects. */
 class Fleet {
   private:
+    /** An array of Boat objects. */
     Boat boats_[MAX_BOATS];
+    /** The number of Boat objects in the collection. */
     int size_;
 
   public:
+    /** Default constructor method. */
     Fleet() {
       size_ = 0;
     }
 
+    /** Returns the number of boats in the fleet. */
     int size() {
       return size_;
     }
 
-    void setSize(int size) {
-      size_ = size;
-    }
-
+    /** Returns a copy of the fleet. */
     Fleet copy();
     
+    /** Adds a Boat object to the fleet. */
     void add(Boat& boat);
 
+    /** Returns the boat at the given index. */
     Boat& boatAtIndex(int index) {
       return boats_[index];
     }
 
+    /** Assignment operator. */
     Fleet& operator=(const Fleet& rhs) {
+      // if the rhs already equals this object, no need to do anything
       if (this != &rhs) {
+        // copy over the attributes of rhs
         size_ = rhs.size_;
         for (int i = 0; i < size_; i++) {
             boats_[i] = rhs.boats_[i];
         }
       }
+      // return the dereferenced pointer
       return (*this);
     }
 };
