c4de8bc3e90b42ec0dded67d09b6a53251c902a7
Maria Kang <maria.kang@ada.ac.uk>
2021-03-10 12:17:20 +0000

    Tidy up CoordinateStack.

--- CoordinateStack.cpp
+++ CoordinateStack.cpp
@@ -1,16 +1,24 @@
 #include "Coordinate.h"
 #include "CoordinateStack.h"
 
+/** Adds a Coordinate object to the collection. */
 void CoordinateStack::push(Coordinate coordinate) {
+  // check that there is sufficient capacity to add another element
   if (size_ < MAX_CAPACITY) {
+    // add the new coordinate to the end, and increment the size
+    stack_[size_] = coordinate;
     size_++;
-    stack_[size_ - 1] = coordinate;
   }
 }
 
+/** Removes the most recently added coordinate from the collection and returns it. */
 Coordinate CoordinateStack::pop() {
+  // reduce the size by 1 (unless already empty)
   if (size_ > 0) {
     size_--;
   }
+  // return the removed element (if the stack was empty to begin with, this will
+  // return the last element to have been removed, but this will never happen in
+  // practice, as the size will be checked before invoking this method)
   return stack_[size_];
 }
--- CoordinateStack.h
+++ CoordinateStack.h
@@ -4,39 +4,52 @@
 
 #define MAX_CAPACITY 6400 // == MAX_ROWS * MAX_COLUMNS
 
+/** A collection of easily accessible Coordinate objects. */
 class CoordinateStack {
   private:
+    /** An array of Coordinate objects. */
     Coordinate stack_[MAX_CAPACITY];
+    /** The number of coordinates currently held. */
     int size_;
 
   public:
+    /** Default constructor method. */
     CoordinateStack() {
       size_ = 0;
     }
 
+    /** Returns the number of coordinates currently held. */
     int size() {
       return size_;
     }
 
+    /** Sets the number of coordinates currently held. */
     void setSize(int size) {
       size_ = size;
     }
-
+ 
+    /** Adds a Coordinate object to the collection. */
     void push(Coordinate coordinate);
 
+    /** Removes the most recently added coordinate from the collection and returns it. */
     Coordinate pop();
   
+    /** Returns the coordinate at the given index. */
     Coordinate coordAtIndex(int index) {
       return stack_[index];
     }
 
+    /** Assignment operator. */
     CoordinateStack& operator=(const CoordinateStack& rhs) {
+      // if the rhs already equals this object, no need to do anything
       if (this != &rhs) {
+        // copy over the attributes of rhs
         size_ = rhs.size_;
         for (int i = 0; i < size_; i++) {
-            stack_[i] = rhs.stack_[i];
+          stack_[i] = rhs.stack_[i];
         }
       }
+      // return the dereferenced pointer
       return (*this);
     }
 };
